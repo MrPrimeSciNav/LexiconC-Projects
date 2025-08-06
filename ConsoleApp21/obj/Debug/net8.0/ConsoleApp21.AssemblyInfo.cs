9e1b8ce5970873fc76b6e5fa7dfa0224cc5798ae
David Nilsson <david822nilsson@gmail.com>
2025-08-06 09:17:19 +0200

    Small modifications of the design of most of the ConsoleApp-projects for C# at Lexicon

--- ConsoleApp21/obj/Debug/net8.0/ConsoleApp21.AssemblyInfo.cs
+++ ConsoleApp21/obj/Debug/net8.0/ConsoleApp21.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp21")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64e7dd354ca08a11b51c76222131fc5cf827b90d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de418d51f5cc5cfdc4f147329b8a2f0708c854d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp21")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp21")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
